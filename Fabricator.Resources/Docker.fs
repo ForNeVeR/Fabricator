8f0c7f04469cc7929ddcb93dac92fb31e2c60805
Friedrich von Never <friedrich@fornever.me>
2023-12-23 00:08:46 +0100

    Docker: add some specification code

---
+++ Fabricator.Resources/Docker.fs
@@ -0,0 +1,30 @@
+﻿module Fabricator.Resources.Docker
+
+open Fabricator.Core
+
+type DockerSourceSpecification =
+    {
+        GitRepository: string
+        GitReference: string
+    }
+
+type DockerOption =
+    | Volume of hostPath: string * containerPath: string
+
+type DockerSpecification =
+    {
+        Sources: DockerSourceSpecification
+        DockerfilePath: string
+        Name: string
+        Tag: string
+        Options: DockerOption seq
+    }
+
+type DockerResource(spec: DockerSpecification) =
+    interface IResource with
+        member this.AlreadyApplied() = failwith "todo"
+        member this.Apply() = failwith "todo"
+        member this.PresentableName = failwith "todo"
+
+let dockerContainer(spec: DockerSpecification): DockerResource =
+    DockerResource spec
